2f4e9251c611289ad57e1806a6129dad6ca1b11b
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-07-26 21:10:26 -0500

    Route parameters

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -8,13 +8,13 @@ var app = express();
 // --> 11)  Mount the body-parser middleware  here
 
 
-/** 1) Meet the node console. 
-console.log("Hello World");*/
+/** 1) Meet the node console.*/
+console.log("Hello World");
 
-/** 2) A first working Express Server
-app.get("/", function(req, res) {
+/** 2) A first working Express Server*/
+app.get("/", function (req, res) {
   res.send("Hello Express");
-})*/
+})
 
 /** 3) Serve an HTML file */
 app.get("/", function (req, res) {
@@ -25,9 +25,9 @@ app.get("/", function (req, res) {
 app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-/*app.get("/json", (req, res) => {
+app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
-});*/
+});
 
 /** 6) Use the .env file to configure the app */
 
@@ -43,16 +43,31 @@ else {
 
 }
 
-
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
+app.get("/json", function (req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
 
 
 /** 8) Chaining middleware. A Time server */
+app.get('/now', function (req, res, next) {
+  req.time = new Date().toString();
+  res.json({ "time": req.time })
+  next();
+}, function (req, res) {
+  res.send(req.time);
+});
 
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", function (req, res, next) {
+  var word = req.params.word;
+  res.json({ "echo": word });
+  next();
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
