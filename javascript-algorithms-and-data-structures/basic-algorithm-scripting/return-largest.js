f9b50dbae496c276cd237f130e27bba29bb01d96
LizethVictoria20 <lizvictoria754@gmail.com>
2021-05-27 14:44:33 +0200

    Return Largest Numbers in Arrays

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest.js
@@ -0,0 +1,11 @@
+// Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+    let lessNumber = 0
+    let biggerNumber = 0
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
