5b59c992a7a1fe495b5e0d23e8a43ce529f610a8
LizethVictoria20 <lizvictoria754@gmail.com>
2021-05-27 15:47:18 +0200

    Largest Numbers in Arrays

--- javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest.js
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest.js
@@ -1,11 +1,24 @@
 // Return Largest Numbers in Arrays
 
 function largestOfFour(arr) {
-    let lessNumber = 0
-    let biggerNumber = 0
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i])
+  let biggerNumber = 0;
+  let newArra = []
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      // console.log(arr[i][j])
+      if (arr[i][j] > biggerNumber) {
+        biggerNumber = arr[i][j];
+      }
     }
+    newArra[i] = biggerNumber
+  }
+  console.log(newArra);
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
+
